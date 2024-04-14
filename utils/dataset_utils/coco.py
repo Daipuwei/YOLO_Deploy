3c15fe78b2ee7b14908136b691136a4d876bc2ce
Daipuwei <771830171@qq.com>
2024-04-14 17:55:11 +0800

    20240414提交代码，增加模型、推理引擎、数据集加载器和TensorRT的INT8量化校准集注册机制，进一步优化代码结构

--- utils/dataset_utils/coco.py
+++ utils/dataset_utils/coco.py
@@ -26,101 +26,56 @@ class COCODataset(Dataset):
             batchsize: 小批量数据集规模，默认为1
             mode: 数据集类型，默认为‘train’,侯选值有['train','val','trainval','test']
         """
-        super(COCODataset,self).__init__(coco_dataset_dir,batchsize,mode)
-        # 初始化相关路径
-        self.train_image_dir = os.path.join(self.dataset_dir,'train')
-        self.val_image_dir = os.path.join(self.dataset_dir,'val')
-        self.test_image_dir = os.path.join(self.dataset_dir,'test')
-        self.test_image_dir = os.path.join(self.dataset_dir, 'test')
-        self.val_json_path = os.path.join(self.dataset_dir, 'annotations', 'val.json')
-        self.test_json_path = os.path.join(self.dataset_dir, 'annotations', 'test.json')
-        if self.mode == "train":
-            self.image_dirs = [self.train_image_dir]
-            self.json_paths = [self.train_json_path]
-        elif self.mode == 'val':
-            self.image_dirs = [self.val_image_dir]
-            self.json_paths = [self.val_json_path]
-        elif self.mode == 'test':
-            self.image_dirs = [self.test_image_dir]
-            self.json_paths = [self.test_json_path]
-        else:
-            self.image_dirs = [self.train_image_dir,self.val_image_dir]
-            self.json_paths = [self.train_json_path,self.val_json_path]
+        meta_data = self.process(coco_dataset_dir,mode)
+        super(COCODataset,self).__init__(meta_data,batchsize)
 
-        # 初始化图像和标签文件路径
-        self.image_gt_dict = {}
-        self.image_id_dict = {}
-        self.id_category_dict = {}
-        for image_dir,json_path in zip(self.image_dirs,self.json_paths):
-            with open(json_path, "r",encoding='utf-8') as f:
-                json_data = json.load(f)
-                image_infos = json_data['images']
-                gts = json_data['annotations']
-                category_id_dict_list = json_data['categories']
-                for _dict in category_id_dict_list:
-                    self.id_category_dict[_dict['id']] = _dict["name"]
-                for image_info in image_infos:
-                    self.image_id_dict[image_info['id']] = os.path.join(image_dir,image_info['file_name'])
-                for gt in gts:
-                    image_id= gt['image_id']
-                    x1,y1,w,h = gt['bbox']
-                    cls_id = gt['category_id']
-                    image_path = self.image_id_dict[image_id]
-                    if image_path not in self.image_gt_dict.keys():
-                        self.image_gt_dict[image_path] = [[self.id_category_dict[cls_id], x1, y1, w, h]]
-                    else:
-                        self.image_gt_dict[image_path].append([self.id_category_dict[cls_id], x1, y1, w, h])
-        self.image_paths = np.array(list(self.image_gt_dict.keys()))
-        self.image_num = len(self.image_paths)
-
-    def __len__(self):
-        size = self.image_num // self.batchsize
-        if self.image_num % self.batchsize != 0:
-            size += 1
-        return size
-
-    def get_batch_data(self,start,end):
+    def process(self, coco_dataset_dir,mode="train"):
         """
-        这是加载一个batchsize数据的函数
+        这是解析coco数据集的函数
         Args:
-            start: 开始索引
-            end: 结束索引
+            coco_dataset_dir: coco数据集地址
+            mode: 子集类型，默认为'train'
         Returns:
         """
-        batch_image_paths = self.image_paths[start:end]
-        batch_meta_data = []
-        batch_images = []
-        for image_path in batch_image_paths:
-            image = cv2.imread(image_path)
-            gts = self.image_gt_dict[image_path]
-            batch_meta_data.append({
+        # 初始化相关路径
+        coco_image_dir = os.path.join(coco_dataset_dir, mode)
+        coco_json_path = os.path.join(coco_dataset_dir, 'annotations', '{}.json'.format(mode))
+
+        # 初始化图像和标签文件路径
+        image_gt_dict = {}
+        image_id_dict = {}
+        id_category_dict = {}
+        with open(coco_json_path, "r", encoding='utf-8') as f:
+            json_data = json.load(f)
+            image_infos = json_data['images']
+            gts = json_data['annotations']
+            category_id_dict_list = json_data['categories']
+            for _dict in category_id_dict_list:
+                id_category_dict[_dict['id']] = _dict["name"]
+            for image_info in image_infos:
+                image_id_dict[image_info['id']] = os.path.join(coco_image_dir, image_info['file_name'])
+            for gt in gts:
+                image_id = gt['image_id']
+                x1, y1, w, h = gt['bbox']
+                cls_id = gt['category_id']
+                image_path = image_id_dict[image_id]
+                if image_path not in image_gt_dict.keys():
+                    image_gt_dict[image_path] = [[id_category_dict[cls_id], x1, y1, w, h]]
+                else:
+                    image_gt_dict[image_path].append([id_category_dict[cls_id], x1, y1, w, h])
+
+        # 初始化meta数据
+        meta_data = []
+        for image_path,gts in image_gt_dict.items():
+            meta_data.append({
+                "image_path": image_path,
                 "gt": gts,
-                "image_path": image_path
             })
-            batch_images.append(image)
-        batch_images = np.array(batch_images)
-        batch_meta_data = np.array(batch_meta_data)
-        return batch_images,batch_meta_data
-
-    def __next__(self):
-        self.end = min(self.start+self.batchsize,self.image_num)
-        if self.end == self.image_num:
-            self.start = 0
-            self.end = 0
-            raise StopIteration
-        else:
-            batch_images,batch_meta_data = self.get_batch_data(self.start,self.end)
-            self.start = self.end
-            return batch_images,batch_meta_data
-
-    def __iter__(self):
-        return self
-
-    def get_image_num(self):
-        return self.image_num
+        meta_data = np.array(meta_data)
+        return meta_data
 
 @DATASET_REGISTRY.register()
-def coco(dataset_dir,batchsize=1,mode='train'):
+def coco(dataset_dir,batchsize=1,mode='val'):
     """
     这是COCO数据集类的注册函数
     Args:
